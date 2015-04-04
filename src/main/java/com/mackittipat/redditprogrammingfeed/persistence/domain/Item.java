2c31f8218150993f8c47c8efffc035874a0c3794
Mac <mac@abctech-thailand.com>
2015-04-04 16:06:19 +0700

    Restore flyway

--- src/main/java/com/mackittipat/redditprogrammingfeed/persistence/domain/Item.java
+++ src/main/java/com/mackittipat/redditprogrammingfeed/persistence/domain/Item.java
@@ -3,7 +3,6 @@ package com.mackittipat.redditprogrammingfeed.persistence.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
@@ -11,10 +10,10 @@ import java.io.Serializable;
 public class Item implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private int id;
 
-    @Column(nullable = false)
+    @Column
     private String title;
 
     public int getId() {
